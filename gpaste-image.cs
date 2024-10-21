89de94498f2fdfd42fd5674246163c06452dc083
gzj <gzj00@outlook.com>
2024-10-21 12:59:12 +0800

    refactor(gpaste-image): add exit code, modify default filename, and return file path

--- gpaste-image.cs
+++ gpaste-image.cs
@@ -2,21 +2,27 @@ using System;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Windows.Forms;
-using System.Collections.Specialized;
 
-class Program {
-
+class Program
+{
     [STAThread]
-    static void Main(string[] args) {
-        string defaultFilename = "clipboard_image.png";
+    static void Main(string[] args)
+    {
+        string defaultFilename = $"clipboard_image_{DateTime.Now:yyyyMMdd_HHmmss}.png";
         string filename = args.Length > 0 ? args[0] : defaultFilename;
 
-        if (Clipboard.ContainsImage()) {
+        if (Clipboard.ContainsImage())
+        {
             Image clipboardImage = Clipboard.GetImage();
             clipboardImage.Save(filename, ImageFormat.Png);
-            Console.WriteLine("Image saved successfully.");
-        } else {
+            string fullPath = System.IO.Path.GetFullPath(filename);
+            Environment.ExitCode = 0;
+            Console.WriteLine(fullPath);
+        }
+        else
+        {
             Console.WriteLine("No image found in the clipboard.");
+            Environment.ExitCode = 1;
         }
     }
 }
